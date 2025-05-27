e495cae740ffe778bf5761aba86f036cabd93b79
seyun0714 <seun0714@naver.com>
2025-05-27 16:00:22 +0900

    Main Page 및 Create Page 구조 마련

---
+++ src/components/common/navigation/SideNavigation.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button";
+import styles from "./SideNavigation.module.scss";
+import { Search } from "lucide-react";
+import React from "react";
+import { Input } from "@/components/ui/input";
+
+function SideNavigation() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.container__searchBox}>
+        <Input
+          type="text"
+          placeholder="검색어를 입력해주세요."
+          className="focus-visible:ring-0"
+        />
+        <Button variant={"outline"} size={"icon"}>
+          <Search className="w-4 h-4" />
+        </Button>
+      </div>
+      <div className={styles.container__buttonBox}>
+        <Button
+          variant={"outline"}
+          className="w-full text-orange-500 border-orange-400 hover:bg-orange-50 hover:text-orange-500"
+        >
+          Add New Page
+        </Button>
+      </div>
+      <div className={styles.container__todos}>
+        <div className={styles.container__todos__label}>Your To Do</div>
+      </div>
+    </div>
+  );
+}
+
+export default SideNavigation;
